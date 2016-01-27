852d7fe0db77f30c34664ec0311252f7d35a9f95
mcxiaoke <mcxiaoke@gmail.com>
2016-01-27 21:19:40 +0800

    tweak async functions

--- core/src/main/kotlin/com/mcxiaoke/koi/core/Core.kt
+++ core/src/main/kotlin/com/mcxiaoke/koi/core/Core.kt
@@ -13,19 +13,31 @@ import java.util.concurrent.Future
  * Date: 16/1/26
  * Time: 17:07
  */
+private object Holder {
+    val mainHandler: Handler by lazy {
+        Handler(Looper.getMainLooper())
+    }
 
-val mainHandler: Handler by lazy {
-    Handler(Looper.getMainLooper())
+    val asyncHandler: Handler by lazy {
+        val t: HandlerThread = HandlerThread("global")
+        t.start()
+        Handler(t.looper)
+    }
+
+    val executor: ExecutorService by lazy {
+        newCachedThreadPool("core")
+    }
 }
 
-val asyncHandler: Handler by lazy {
-    val t: HandlerThread = HandlerThread("global")
-    t.start()
-    Handler(t.looper)
+val mainHandler by lazy {
+    Holder.mainHandler
+}
+val asyncHandler by lazy {
+    Holder.asyncHandler
 }
 
-val executor: ExecutorService by lazy {
-    newCachedThreadPool("global")
+val executor by lazy {
+    Holder.executor
 }
 
 inline fun <T> callable(crossinline action: () -> T?): Callable<out T> {
@@ -36,21 +48,38 @@ inline fun runnable(crossinline action: () -> Unit): Runnable {
     return Runnable { action() }
 }
 
-fun main(action: () -> Unit, delayMillis: Long = 0): Boolean
+fun delay(action: () -> Unit, delayMillis: Long = 0): Boolean
         = mainHandler.postDelayed(action, delayMillis)
 
-fun async(action: () -> Unit, executor: ExecutorService): Future<out Any?> {
-    return executor.submit(action)
+fun async(action: () -> Unit, executor: ExecutorService): Future<Unit> {
+    return executor.submit<Unit>(action)
 }
 
-fun async(action: () -> Unit): Unit = executor.execute(action)
+fun async(action: () -> Unit): Future<Unit> = executor.submit<Unit>(action)
 
 inline fun <T> async(crossinline action: () -> T?,
-                     crossinline callback: (result: T?) -> Unit): Unit {
-    executor.submit {
-        val ret: T? = action()
+                     crossinline callback: (result: T?) -> Unit): Future<*> {
+    return executor.submit {
+        val ret = action()
         mainHandler.post {
             callback(ret)
+        }
+    }
+}
+
+inline fun <T> async(crossinline action: () -> T?,
+                     crossinline success: (result: T?) -> Unit,
+                     crossinline failure: (error: Throwable) -> Unit): Future<*> {
+    return executor.submit {
+        try {
+            val ret = action()
+            mainHandler.post {
+                success(ret)
+            }
+        } catch(e: Exception) {
+            mainHandler.post {
+                failure(e)
+            }
         }
     }
 }
