94e40a264faa89d17f01a71aa8b529c8cefda9df
mcxiaoke <mcxiaoke@gmail.com>
2016-02-04 09:26:27 +0800

    add view extensions sample

--- samples/src/main/kotlin/com/mcxiaoke/koi/samples/ViewSample.kt
+++ samples/src/main/kotlin/com/mcxiaoke/koi/samples/ViewSample.kt
@@ -1,17 +1,82 @@
 package com.mcxiaoke.koi.samples
 
+import android.app.Activity
 import android.content.Context
+import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 import com.mcxiaoke.koi.ext.*
+import org.jetbrains.anko.onKey
+import org.jetbrains.anko.onTouch
 
 /**
  * Author: mcxiaoke
  * Date:  2016/2/2 23:06
  */
 
-class ViewExtensionSample(ctx: Context) : View(ctx) {
+class ViewExtensionSample1 : Activity() {
+
+    fun viewListeners1() {
+        val view = View(this)
+        // View.OnClickListener
+        view.onClick { print("view clicked") }
+        // View.OnLongClickListener
+        view.onLongClick { print("view long clicked");false }
+        // View.OnKeyListener
+        view.onKeyEvent { view, keyCode, event ->
+            print("keyEvent: action:${event.action} code:$keyCode")
+            false
+        }
+        // View.OnTouchListener
+        view.onTouchEvent { view, event ->
+            when (event.actionMasked) {
+                MotionEvent.ACTION_DOWN -> print("touch down")
+                MotionEvent.ACTION_UP -> print("touch up")
+                MotionEvent.ACTION_MOVE -> print("touch move")
+            }
+            false
+        }
+        // View.OnFocusChangeListener
+        view.onFocusChange { view, hasFocus ->
+            print("focus changed = $hasFocus")
+        }
+    }
+
+    fun viewListeners2() {
+        // TextWatcher
+        val editText = EditText(this)
+        editText.onTextChange { text, start, before, count ->
+            print("text changed: $text")
+        }
+
+        // OnCheckedChangeListener
+        val checkBox = CheckBox(this)
+        checkBox.onCheckedChanged { button, isChecked ->
+            print("CheckBox value changed:$isChecked")
+        }
+
+        // OnSeekBarChangeListener
+        val seekBar = SeekBar(this)
+        seekBar.onProgressChanged { seekBar, progress, fromUser ->
+            print("seekBar progress: $progress")
+        }
+    }
+
+    fun listViewListeners() {
+        val listView = ListView(this)
+        // OnItemClickListener
+        listView.onItemClick { parent, view, position, id ->
+            print("onItemClick: position=$position")
+        }
+        // OnScrollListener
+        listView.onScrollChanged { view, scrollState ->
+            print("scroll state changed")
+        }
+    }
+}
+
+class ViewExtensionSample2(ctx: Context) : View(ctx) {
 
 
     // available for View
